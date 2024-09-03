84999337ad710652f2ce837e443f12d89a80db8c
PhuongPham7112 <phuongp@usc.edu>
2024-09-02 19:25:08 -0700

    mass spring system

--- airi-2d-fem/starterCode/massSpring2D.cpp
+++ airi-2d-fem/starterCode/massSpring2D.cpp
@@ -40,27 +40,65 @@ void MassSpring2D::ComputeElementEnergyAndForceAndStiffnessMatrix(int eid, const
   double stiffness = mesh->getMassSpringStiffness(); // stiffness of the spring
   int particleA = edges[2*eid+0]; // index of vertex A on the spring
   int particleB = edges[2*eid+1]; // index of vertex B on the spring
-  // undeformedPositions, size 2*vertexNumber, stores the rest positions of all verteices
-  // u, size 2*vertexNumber, stores the displacements of all verteices
+  // undeformedPositions, size 2*vertexNumber, stores the rest positions of all vertices
+  // u, size 2*vertexNumber, stores the displacements of all vertices
 
   // ********** Students should implement this **********
+
+  // point a: rest position, displacement, deformed position
+  Vec2d x_a = Vec2d(undeformedPositions[particleA * 2], undeformedPositions[particleA * 2 + 1]);
+  Vec2d u_a = Vec2d(u[particleA * 2], u[particleA * 2 + 1]);
+  Vec2d p_a = x_a + u_a;
+
+  // point b: rest position, displacement, deformed position
+  Vec2d x_b = Vec2d(undeformedPositions[particleB * 2], undeformedPositions[particleB * 2 + 1]);
+  Vec2d u_b = Vec2d(u[particleB * 2], u[particleB * 2 + 1]);
+  Vec2d p_b = x_b + u_b;
   
+  // vector L
+  Vec2d L = p_a - p_b;
+  double l_length = length(L);
+  double r_length = restLengths[eid];
+  Vec2d f_a = -stiffness * (l_length - r_length) * norm(L);
+  Vec2d f_b = -f_a;
+  
+  double x = stiffness * (1.0 - r_length * ((1 / l_length) - (1 / pow(l_length, 3.0)) * (p_a[0] - p_b[0]) * (p_a[0] - p_b[0])));
+  double y = stiffness * (0.0 - r_length * (-(1 / pow(l_length, 3.0)) * (p_a[0] - p_b[0]) * (p_a[1] - p_b[1])));
+  double z = stiffness * (1.0 - r_length * ((1 / l_length) - (1 / pow(l_length, 3.0)) * (p_a[1] - p_b[1]) * (p_a[1] - p_b[1])));
+
   if (elementEnergy)
   {
     // please calculate the elastic energy on the spring here
-    
+      *elementEnergy = 0.5 * stiffness * (l_length - r_length) * (l_length - r_length);
   }
-
-  if (elementInternalForces)
+  if (elementInternalForces) // 2x2 matrix
   {
     // please calculate the the spring forces here
-   
+      elementInternalForces[0] = f_a[0];
+      elementInternalForces[1] = f_a[1];
+      elementInternalForces[2] = f_b[0];
+      elementInternalForces[3] = f_b[1];
   }
 
-  if (elementStiffnessMatrix) 
+  if (elementStiffnessMatrix)  // 4x4 matrix
   {
     // please calculate the stiffness matrix for the spring here
-    
+      //int mod = 1;
+      //for (int i = 0; i < 4; i++)
+      //{
+      //    if (i == 1 || i == 2)
+      //    {
+      //        mod = -1;
+      //    }
+      //    else
+      //    {
+      //        mod = 1;
+      //    }
+      //    elementStiffnessMatrix[i * 4] = mod * x;
+      //    elementStiffnessMatrix[i * 4 + 1] = mod * y;
+      //    elementStiffnessMatrix[i * 4 + 2] = mod * y;
+      //    elementStiffnessMatrix[i * 4 + 3] = mod * z;
+      //}
   }
 
   // ***********************************************
--- airi-2d-fem/starterCode/vega/forceModelAssembler2D.cpp
+++ airi-2d-fem/starterCode/vega/forceModelAssembler2D.cpp
@@ -97,7 +97,7 @@ ForceModelAssembler2D::ForceModelAssembler2D(StencilForceModel *eleFM) : stencil
   for (int eltype = 0; eltype < stencilForceModel->GetNumStencilTypes(); eltype++) 
   {
     int nelev = stencilForceModel->GetNumStencilVertices(eltype);
-    bufferExamplars[eltype].resize(nelev * 2 + nelev * nelev * 4);
+    bufferExamplars[eltype].resize(nelev * 2 + nelev * nelev * 4); // contain both itnernal forces and stiffness matrix
   }
 }
 
